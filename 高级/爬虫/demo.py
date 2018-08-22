ceb2b76ff1df9a16e8b805998a85c84eab61868e
weixinhuhu <weixin@rthitech.com.cn>
2018-08-22 13:38:08 +0800

    20180822

--- 高级/爬虫/demo.py
+++ 高级/爬虫/demo.py
@@ -1,11 +1,15 @@
 import urllib.request
 import urllib.parse
 import urllib.request, urllib.parse, http.cookiejar,re
+from typing import List, Any
+
 from bs4 import BeautifulSoup
 
-def getHtml(url):
+def gethtml(url):
     cj = http.cookiejar.CookieJar()
+
     opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+
     opener.addheaders = [('User-Agent',
                           'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36'),
                          ('Cookie', '4564564564564564565646540')]
@@ -13,29 +17,30 @@ def getHtml(url):
     urllib.request.install_opener(opener)
 
     html_bytes = urllib.request.urlopen(url).read()
+
     html_string = html_bytes.decode('utf-8')
+
     return html_string
 
-html = getHtml("http://zst.aicai.com/ssq/openInfo/")
+html_url = gethtml("http://zst.aicai.com/ssq/openInfo/")
 
-soup=BeautifulSoup(html,"html.parser")
+soup=BeautifulSoup(html_url, "html.parser")
 
 # print(soup.prettify())
 
-StrDate=soup.find(onmouseover="this.style.background='#fff7d8'")
+Str_Date=soup.find(onmouseover="this.style.background='#fff7d8'")
 
-datestr=StrDate.findAll("td")[0]
+date_str = Str_Date.findAll("td")[0]
 
-red=StrDate.findAll("td")[2:8]
+red = Str_Date.findAll("td")[2:8]
 
-blue=StrDate.findAll("td")[8]
+blue = Str_Date.findAll("td")[8]
 
-l= [redboll.getText() for redboll in red]
+print(date_str.getText()+" 期中奖号码： ")
 
-print(datestr.getText()+" 期中奖号码： ")
+for s in red:
 
-for s in l:
-    print('\033[1;31m'+s)
+    print('\033[1;31m'+s.getText())
 
 print('\033[1;34m' + blue.getText() + '\033[0m')
 
